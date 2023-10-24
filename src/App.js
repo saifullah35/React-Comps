ac8d448b1fce24fe6fe305a724475e9ea518819d
Saif <saif@Arifs-MBP-4.lan>
2023-10-24 12:48:11 -0400

    Using ClassNames library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import Button from './Button';
 
 function App () {
-    return <div>
+    return (
         <div>
-            <Button>Click me!!</Button>
+            <div>
+                <Button success rounded outline>
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    Buy now!
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    See Deal!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Hide ads!
+                </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    Something
+                </Button>
+            </div>
         </div>
-        <div>
-            <Button>Buy now!</Button>
-        </div>
-        <div>
-            <Button>See Deal!</Button>
-        </div>
-        <div>
-            <Button>Hide ads!</Button>
-        </div>
-        <div>
-            <Button>Something</Button>
-        </div>
-    </div>
-};
+    );
+}
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
+import { twMerge } from 'tailwind-merge';
 
 function Button( { 
     children,
@@ -10,7 +11,19 @@ function Button( {
     outline,
     rounded
 }) {
-    return <button>{children}</button>;
+    const classes = twMerge(
+        className('px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        })
+    );
+
+    return (
+        <button className= {classes}>{children}</button>
+    );
 }
 
 Button.propTypes = {
