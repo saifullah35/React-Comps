1e6a8d7ebac57375f0a4849686f5fcc2c71c6679
Saif <saif@Arifs-MBP-4.lan>
2023-10-25 12:54:03 -0400

    Custom navigation and routing system section comeplete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 function App () {
+    const handleClick = () => {
+        console.log('Click!!');
+    };
+
     return (
         <div>
             <div>
-                <Button success rounded outline>
+                <Button success rounded outline className="mb-5" onClick={handleClick}>
+                    <GoBell />
                     Click me!!
                 </Button>
             </div>
             <div>
-                <Button danger outline>
+                <Button danger outline onMouseEnter={handleClick}>
+                    <GoCloudDownload />
                     Buy now!
                 </Button>
             </div>
             <div>
-                <Button warning>
+                <Button warning onMouseLeave={handleClick}>
+                    <GoDatabase />
                     See Deal!
                 </Button>
             </div>
