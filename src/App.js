6969618474cd001713a3ac457561808898ff99b9
Saif <saif@Arifs-MBP-4.lan>
2023-10-25 13:57:08 -0400

    Finding the expanded item

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Accordion from './components/Accordion';
 function App() {
     const item = [
         {
+            id: 'l2kj5',
             label: 'Can I use React on a project?',
             content:'You can use React on any project you want.'
         },
         {
+            id: 'lab125',
             label: 'Can I use JavaScript on a project?',
             content:'You can use React on any project you want.'
         },
         {
+            id: 'cd321',
             label: 'Can I use CSS on a project?',
             content:'You can use React on any project you want.'
         }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,28 @@
-function Accordion( { items } ) {
-    return <div/>;
+import { useState } from "react";
+
+function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        );
+    });
+
+    return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
+
+
+// The Accordion component is simple: just expands and collapses different section
+
+// Anytime we change content on the screen, we need to use the STATE system
+// What should State be called
+// What type of data will my State be
+// Which component should State be defined in
