4e121efd0a2cbb3e2e90817ad389c275db2543cd
Saif <saif@Arifs-MBP-4.lan>
2023-10-25 14:39:23 -0400

    Variation on event handlers

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,16 @@ import { useState } from "react";
 function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex);
+    };
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         return (
             <div key={item.id}>
-            <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+            <div onClick={() => handleClick(index)}>{item.label}</div>
             {isExpanded && <div>{item.content}</div>}
             </div>
         );
@@ -26,3 +30,6 @@ export default Accordion;
 // What should State be called
 // What type of data will my State be
 // Which component should State be defined in
+
+// onClick={() => handleClick(index)} --> This arrow function when executed is going to call handleClick
+// and pass in the current value of the index*/
