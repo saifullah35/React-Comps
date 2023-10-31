71611eb99854726d1447f105882a95e61cd89367
Saif <saif@Arifs-MBP-4.lan>
2023-10-31 13:37:26 -0400

    controlled component implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+    const [selection, setSelection] = useState(null); // null because no item is currently selected
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
     const options = [
         { label: 'Red', value: 'red'},
         { label: 'Green', value: 'green'},
         { label: 'Blue', value: 'blue'},
     ];
 
-    return <Dropdown options={options} />
+    return (
+        <Dropdown 
+        options={options} 
+        selection={selection} 
+        onSelect={hanldeSelect}
+        />
+    );
 }
 
 export default App;
