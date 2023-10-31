781cf2f25ee545ee5382dd101d186f5e6f1dcbb3
Saif <saif@Arifs-MBP-4.lan>
2023-10-31 13:27:25 -0400

    review on event handlers

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,16 +7,26 @@ function Dropdown({ options }) {
         setIsOpen(!isOpen);
     };
 
+    const handleOptionClick = (option) => {
+        // Closes ndropdown: update the value of piece of state
+        setIsOpen(false);
+        // What option did user click on
+        console.log(option);
+    };
+
     const renderedOptions = options.map((option) => {
-        return <div key={option.value}>{option.label}</div>
+        return <div onClick={handleOptionClick}key={option.value}>{option.label}</div>
     });
 
     return (
         <div>
-            <div onClick={handleClick}>Select...</div>
+            <div onClick={() => handleClick}>Select...</div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
 }
 
 export default Dropdown;
+
+// Communicate some info from inside the mapping function to an event handler
+// Whenevr user calls this arrow function () =>, we invoke hanldeOptionClick
