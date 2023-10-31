3b20bf809528f2772deda29242dd43d4aaf5d3cb
Saif <saif@Arifs-MBP-4.lan>
2023-10-31 14:06:40 -0400

    creating the reusable panel

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -25,17 +26,17 @@ function Dropdown({ options, value, onChange }) {
 
     return (
         <div className="w-48 relative">
-            <div 
-                className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" 
+            <Panel
+                className="flex justify-between items-center cursor-pointer" 
                 onClick={handleClick}
             >
                 {value?.label || 'Select...'}
                 <GoChevronDown className="text-lg"/>
-            </div>
+            </Panel>
             {isOpen && (
-            <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+            <Panel className="absolute top-full">
                 {renderedOptions}
-            </div>
+            </Panel>
             )};
         </div>
     );
