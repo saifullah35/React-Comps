c0f9a0233e0067ee4a46e1e6ffd8398484bfb575
Saif <saif@Arifs-MBP-4.lan>
2023-10-31 16:24:34 -0400

    everything up to date

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-    const [selection, setSelection] = useState(null); // null because no item is currently selected
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        { label: 'Red', value: 'red'},
-        { label: 'Green', value: 'green'},
-        { label: 'Blue', value: 'blue'},
-    ];
-
-    return (
-        <div className="flex">
-        <a href="/dashboard">Dashboard</a>
-        <Dropdown 
-        options={options} 
-        value={selection} 
-        onChange={handleSelect}
-        />
-        <Dropdown 
-        options={options} 
-        value={selection} 
-        onChange={handleSelect}
-        />
-        </div>
-    );
+    <div></div>
 }
 
 export default App;
