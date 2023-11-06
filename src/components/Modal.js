b14bf6db6bb71fa66908f53b7d8ad872bc34bbe1
Saif <saif@Arifs-MBP-4.lan>
2023-11-06 16:49:03 -0500

    Closing the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
 import { ReactDOM } from "react-dom";
 
-function Modal() {
+function Modal({ onClose }) {
     return ReactDOM.createPortal(
         <div>
-            <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+            <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
             <div classname="absolute inset-40 p-10 bg-white">I'm a modal</div>
         </div>,
         document.querySelector('.modal-container')
