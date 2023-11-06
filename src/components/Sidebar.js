ad2fe6a18822cb4406c35653dc614df5e21f201a
Saif <saif@Arifs-MBP-4.lan>
2023-11-06 16:14:48 -0500

    hghlighting the active link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,11 +9,20 @@ function Sidebar() {
     ];
 
     const renderedLinks = links.map((link) => {
-        return <Link key={link.label} to={link.path}>{link.label}</Link>
+        return (
+            <Link 
+            key={link.label} 
+            to={link.path} 
+            className="mb-3"
+            activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+            >
+                {link.label}
+            </Link>
+        );
     });
 
     return (
-        <div classname="sticky top-0 overflow-y-scroll flex flex-col">
+        <div classname="sticky top-0 overflow-y-scroll flex flex-col items-start">
             {renderedLinks}
         </div>
     );
