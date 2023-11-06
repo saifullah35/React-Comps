c218716fa24431c1dde19c375621c1a199682012
Saif <saif@Arifs-MBP-4.lan>
2023-11-06 16:27:35 -0500

    Toggling visibility

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,20 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
+    const [ showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    }
+
     return (
         <div>
-            <Modal />
+            <Button onClick={handleClick} primary>
+                Open Modal
+            </Button>
+            {showModal && <Modal />}
         </div>
     );
 }
