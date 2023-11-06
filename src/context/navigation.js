f9de063d1210c9f60914da10c3c9f265209b54b3
Saif <saif@Arifs-MBP-4.lan>
2023-11-06 15:28:38 -0500

    listening to forward and back clicks

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider() {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname); // currentPath piece of state to have a default value when it's first declared
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        };
+    }, []);
+
     return (
             <NavigationContext.Provider value={{}}>
+                {currentPath}
                 {children}
             </NavigationContext.Provider>
         );
