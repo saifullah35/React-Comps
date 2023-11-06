92c6a93fb269aa5ede3ae9e3ac9b381e9dba4d5a
Saif <saif@Arifs-MBP-4.lan>
2023-11-06 17:18:50 -0500

    HTML Table structure

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,18 @@
 function Table({ data }) {
-    return <div>{data.length}</div>
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    );
 }
 
 export default Table;
