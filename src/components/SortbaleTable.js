cbf872a76b5c8a43df5d9e6e995a3caa6c904b66
Saif <saif@Arifs-MBP-4.lan>
2023-11-07 15:58:09 -0500

    stlying header cells

--- src/components/SortbaleTable.js
+++ src/components/SortbaleTable.js
@@ -1,5 +1,6 @@
 import Table from "./Table";
 import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 
 function SortableTable(props) {
     const [sortOrder, setSortOrder] = useState(null);
@@ -27,9 +28,14 @@ function SortableTable(props) {
         return {
             ...column,
             header: () => (
-                <th onClick={() => handleClick(column.label)}>
-                    {getIcons(column.label, sortBy, sortOrder)}
-                    {column.label} 
+                <th 
+                    className="cursor-pointer hover:bg-gray-100" 
+                    onClick={() => handleClick(column.label)}
+                >
+                    <div className="flex items-center">
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label} 
+                    </div>
                 </th>
             ),
         };
@@ -66,15 +72,33 @@ function SortableTable(props) {
 
 function getIcons(label, sortBy, sortOrder) {
     if (label !== sortBy) {
-        return 'Show both icons';
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
     }
 
     if (sortOrder === null) {
-        return 'Show both icons';
+        return (
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
     } else if (sortOrder === 'asc') {
-        return 'Show up icon';
+        return (
+            <div>
+                <GoArrowSmallUp />
+            </div>
+        );
     } else if (sortOrder === 'desc') {
-        return 'Show down icon';
+        return (
+            <div>
+                <GoArrowSmallDown />
+            </div>
+        );
     }
 }
 
