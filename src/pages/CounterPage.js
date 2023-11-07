b13aeffb2890e146047fdc68ba7ba3207b6e66b6
Saif <saif@Arifs-MBP-4.lan>
2023-11-07 16:21:29 -0500

    Hook creation Process

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,21 +1,33 @@
 import { useState, useEffect } from "react";
 import Button from '../components/Button';
 
-function CounterPage({ initialCount }) {
+function useCounter(initialCount) {
+    // Hook that creates a peice of state that's gonna be a number
     const [count, setCount] = useState(initialCount);
 
+    // Logs out that value every time it changes
     useEffect (() => {
         console.log(count);
     }, [count]);
 
-    const handleClick = () => {
+    // Provides a way to change that state
+    const increment = () => {
         setCount(count + 1);
     };
+
+    return {
+        count,
+        increment
+    };
+}
+
+function CounterPage({ initialCount }) {
+    const { count, increment } = useCounter(initialCount);
 
     return (
         <div>
             <h1>Count is {count}</h1>
-            <Button onClick={handleClick}>
+            <Button onClick={increment}>
                 Increment
             </Button>
         </div>
